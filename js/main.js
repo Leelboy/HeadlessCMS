c3615d04e39036929b304db1e7bd204353359580
Jiwan <jkim472@myseneca.ca>
2024-04-10 09:09:39 +0900

    .
    .

--- js/main.js
+++
@@ -1,9 +0,0 @@
-console.log('Web Development - Headless CMS');
-
-var obj = '{"name": "Sachin", "age":30, "car":null}';
-let parsedObj = JSON.parse(obj);
-console.log('parseObj: ', parsedObj);
-
-var test = { "name": "Sachin", "age": 30, "car": null };
-let parsedTest = JSON.stringify(test);
-console.log("string test: ", parsedTest);
